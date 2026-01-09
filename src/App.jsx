473abdd5d7e9f76e672f687e9707389567895063
Dhyan U P <dhyanup2003@gmail.com>
2026-01-09 11:08:04 +0530

    Update header text from 'DHYAN' to 'DHYAN UP'

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>DHYAN</h1>
+      <h1>DHYAN UP</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
